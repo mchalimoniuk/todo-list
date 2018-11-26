ec50848f7f47596991ac3dfb600aa00e07c7826d
mchalimoniuk <michalchalimoniuk29@gmail.com>
2018-11-26 08:54:15 +0100

    Styled delete todos button

--- src/components/TodoDeleteCompeletedBtn.js
+++ src/components/TodoDeleteCompeletedBtn.js
@@ -4,6 +4,20 @@ import { TODOS_DELETE_START } from '../constants/ActionTypes';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const DeleteCompletedBtn = styled.button`
+  width: 100%;
+  background: none;
+  color: #f47382;
+  border-color: #f47382;
+  font-size: 20px;
+  padding: 10px;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 class TodoDeleteCompeletedBtn extends Component {
   dispatchDeleteAllCompletedTodos() {
@@ -15,7 +29,7 @@ class TodoDeleteCompeletedBtn extends Component {
 
   render() {
     return (
-      <button onClick={this.dispatchDeleteAllCompletedTodos.bind(this)}>Usuń zakończone zadania</button>
+      <DeleteCompletedBtn onClick={this.dispatchDeleteAllCompletedTodos.bind(this)}>Usuń zakończone zadania</DeleteCompletedBtn>
     );
   }
 }
